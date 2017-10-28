1961d2d1e496d4087781e82fec8b85e4cb0c963c
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-10-28 13:33:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+new Vue({
+
+	el : '#vue-app-one',
+
+
+	data : {
+		output : 'Your fav faood'
+	},
+
+
+	methods : {
+
+		readRefs : function(){
+
+			console.log(this.$refs);
+
+			this.output = this.$refs.input.value
+		}
+
+	}
+
+
+});
