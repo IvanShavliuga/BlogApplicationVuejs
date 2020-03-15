f3c1a5adfa23511ab7c234c54091b5ebf5564be5
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-03-15 16:56:48 +0300

    Delete spaces

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 new Vue({
-
 	el : '#vue-app-one',
-
-
 	data : {
 		output : 'Your fav faood'
 	},
-
-
 	methods : {
-
 		readRefs : function(){
-
 			console.log(this.$refs);
-
 			this.output = this.$refs.input.value
 		}
-
 	}
-
-
 });
--- src/components/header.vue
+++ src/components/header.vue
@@ -6,55 +6,32 @@
 		</ul>
 	</nav>
 </template>
-
-
-
 <script>
-
 	export default {}
-
 </script>
-
-
 <style scoped>
-	
-	ul{
-	
+	ul{	
 		list-style-type: none;
 		text-align: center;
 		margin: 0;
 	}
-
 	li {
-
 		display: inline-block;
-
 		margin : 0 10px;
-
 	}
-
 	a {
 		color : #fff;
-
 		text-decoration: none;
-
 		padding: 6px 8px;
-
 		border-radius: 10px;
 	}
-
 	nav {
-
 		background: #444;
 		padding: 14px 0;
 		margin-bottom: 40px;
-
 	}
-
 	.router-link-active{
-
 		background-color: #eee;
-		color : #444;
-	
+		color : #444;	
 	}
 </style>
